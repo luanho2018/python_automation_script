fb4c3e80d0cd94663934d9d7ffdc59b47647be81
Luan Ho <lqh24430@uga.edu>
2023-07-29 17:32:35 -0400

    updated automation.py to run continously until stopped

--- automation.py
+++ automation.py
@@ -1,14 +1,20 @@
 import os
 import shutil
+import time
 
-download_path = ('/Users/luanho/Desktop')
+desktop_path = ('/Users/luanho/Desktop')
 photos_path = ('/Users/luanho/Desktop/Photos')
 
-with os.scandir(download_path) as entries:
+# lists all files in desktop
+with os.scandir(desktop_path) as entries:
     for entry in entries:
         print(entry.name)
 
-for filename in os.listdir(download_path):
-    if filename.lower().endswith(('.png', '.jpeg', '.jpg')):
-        file_path = os.path.join(download_path, filename)
-        shutil.move(file_path, photos_path)
+while True:
+    # for all files in the desktop, if they are a picture, move them into photos
+    for filename in os.listdir(desktop_path):
+        if filename.lower().endswith(('.png', '.jpeg', '.jpg')):
+            file_path = os.path.join(desktop_path, filename)
+            shutil.move(file_path, photos_path)
+    print("This code is running")
+    time.sleep(1)
